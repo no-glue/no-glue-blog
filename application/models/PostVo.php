551d3ea638a4e8a74ca814a76daa2f2f1c5ad120
Nikola <nikola@ubuntu.ubuntu-domain>
2013-03-25 10:50:43 +0100

    Added post model.

--- application/models/PostVo.php
+++ application/models/PostVo.php
@@ -14,7 +14,47 @@ class PostVo{
 		$this->_id=$id;
 	}
 
-	public function getId($id){
+	public function getId(){
 		return $this->_id;
+	}
+
+	public function setName($name){
+		$this->_name=$name;
+	}
+
+	public function getName(){
+		return $this->_name;
+	}
+
+	public function setTitle($title){
+		$this->_title=$title;
+	}
+
+	public function getTitle(){
+		return $this->_title;
+	}
+
+	public function setBody($body){
+		$this->_body=$body;
+	}
+
+	public function getBody(){
+		return $this->_body;
+	}
+
+	public function setCreated($created){
+		$this->_created=$created;
+	}
+
+	public function getCreated(){
+		return $this->_created;
+	}
+
+	public function setModified($modified){
+		$this->_modified=$modified;
+	}
+
+	public function getModified(){
+		return $this->_modified;
 	}
 }
