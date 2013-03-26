e37e1ed618446c2c309e0542ab4a9e826262fde3
Nikola <nikola@ubuntu.ubuntu-domain>
2013-03-26 16:12:30 +0100

    Fixed index routes observer to show correct pages.

--- premade/IndexRoutesObserver.php
+++ premade/IndexRoutesObserver.php
@@ -9,11 +9,12 @@ class IndexRoutesObserver{
 	public function update($index){
 		$routes=$index->getRoutes();
 
-		if($routes['class']&&$routes['action']){
+		if($routes['class']&&$routes['action']&&!is_null($routes['params'])){
 			$application=require_once($routes['folder'].'/'.$routes['class'].'.php');
 		} else {
 			$application=require_once($routes['folder'].'/PageNotFound.php');
 			$routes['action']='index';
+			$routes['params']=array();
 		}
 
 		call_user_func_array(array($application,$routes['action']),$routes['params']);
