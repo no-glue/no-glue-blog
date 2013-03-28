7f9868fe2562c6c19fc3dbe98ed29cbf54a93f67
Nikola <nikola@ubuntu.ubuntu-domain>
2013-03-28 15:35:13 +0100

    Added validation rules.

--- application/models/ValidationRulesRefinery.php
+++ application/models/ValidationRulesRefinery.php
@@ -5,11 +5,12 @@ namespace application\models;
 require_once('ValidationRulesFactory.php');
 
 class ValidationRulesRefinery{
-	public static function refine($object='\application\models\ValidationRules'){
-		$instance=ValidationRulesFactory::create($object);
+	public static function refine(){
+		$argumentsList=func_get_args();
+		$instance=ValidationRulesFactory::create($argumentsList[0]);
 
 		if(func_num_args()>1){
-			call_user_func_array(array($instance,'keep'),array_slice(func_get_args(),1));
+			call_user_func_array(array($instance,'keep'),array_slice($argumentsList,1));
 		}
 
 		return $instance;
