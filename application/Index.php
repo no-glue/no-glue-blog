512d384ec3d0eee63bd1993fb1b29941aa1c1681
Nikola <nikola@ubuntu.ubuntu-domain>
2013-03-31 20:51:40 +0200

    Removed tests from application index.

--- application/Index.php
+++ application/Index.php
@@ -2,16 +2,8 @@
 
 namespace application;
 
-require_once('models/PostDao.php');
-
-use application\models;
-
 class Index{
-	protected $_postDao;
-
-	public function __construct($postDao){
-		$this->_postDao=$postDao;
-	}
+	public function __construct(){}
 
 	public function index(){
 		require_once('views/index_index.php');
@@ -20,36 +12,6 @@ class Index{
 	public function testParams($paramFirst){
 		var_dump($paramFirst);
 	}
-
-	public function testDatabase(){
-		var_dump($this->_postDao->getPosts());
-	}
-
-	public function testValidationPositive(){
-		var_dump($this->_postDao->validateAndSave(new \application\models\PostVo(
-			NULL,
-			'far-far-new',
-			'far far away',
-			'far far away',
-			time(),
-			time()
-		)));
-	}
-
-	public function testValidationNegative(){
-		var_dump($this->_postDao->validateAndSave(new \application\models\PostVo(
-			NULL,
-			'',
-			'far far away',
-			'far far away',
-			time(),
-			time()
-		)));
-	}
 }
 
-return new \application\Index(
-	new \application\models\PostDao(
-		\premade\DatabaseConnectionFactory::create()
-	)
-);
+return new \application\Index();
