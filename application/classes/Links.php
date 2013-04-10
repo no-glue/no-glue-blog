67b8c94908c7a46f9345227502888f60054b56eb
nikola <nikola@nikola-vi.(none)>
2013-04-10 23:36:38 +0200

    Changed links helper use.

--- application/classes/Links.php
+++ application/classes/Links.php
@@ -5,10 +5,15 @@ namespace application\classes;
 require_once('ConfigureLoader.php');
 
 class Links{
-	public static function link($class,$action,$text,$parameters=array(),$host='',$helper='config_loader'){
-		$helper=self::_helper($helper);
+	protected static function _helper($helper,$helpers=array(
+		'configure_loader'=>'\application\classes\ConfigureLoader')){
+		return $helpers[$helper];
+	}
+
+	public static function link($class,$action,$text,$parameters=array(),$host=''){
+		$configureLoader=self::_helper('configure_loader');
 		$host=(string)$host;
-		$host=($host==='')?$helper::help():$host;
+		$host=($host==='')?$configureLoader::help():$host;
 
 		$result='<a href=\''.$host.'?class='.$class.'&action='.$action;
 
@@ -25,10 +30,5 @@ class Links{
 
 	public static function css($location,$type='text/css',$rel='stylesheet'){
 		return '<link href=\''.$location.'\' type=\''.$type.'\' rel=\''.$rel.'\'/>';
-	}
-
-	protected static function _helper($helper,$helpers=array(
-		'config_loader'=>'\application\classes\ConfigureLoader')){
-		return $helpers[$helper];
 	}
 }
