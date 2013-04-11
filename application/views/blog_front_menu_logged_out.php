dcc780b99d49ade6f90598a763a1955dd197e4e3
nikola <nikola@nikola-vi.(none)>
2013-04-11 23:29:20 +0200

    Added link for contacts.

--- application/views/blog_front_menu_logged_out.php
+++ application/views/blog_front_menu_logged_out.php
@@ -1,4 +1,4 @@
 <span class="menu_item text_shadow"><a href='#'>english</a></span>
 <span class="menu_item text_shadow"><a href='#'>srpski</a></span>
-<span class="menu_item text_shadow"><a href='#'>contact</a></span>
+<span class="menu_item text_shadow"><?php echo \application\classes\Links::link('blog_front_contacts','index','contact');?></span>
 <span class="menu_item text_shadow"><?php echo \application\classes\Links::link('blog_front_posts','index','projects');?></span>
