89c38a0bff0906c58dccd9b7f820394fd519ea4d
Nikola <nikola@localhost.localdomain>
2013-04-21 17:37:57 +0200

    Added posts controller.

--- application/BlogAdminPosts.php
+++ application/BlogAdminPosts.php
@@ -4,16 +4,16 @@ namespace application;
 
 use application\classes;
 
-class BlogAdminIndex{
+class BlogAdminPosts{
 	public function __construct(){}
 
 	public function index(){
 		require_once('classes/View.php');
 
 		\application\classes\View::load('blog_admin_template.php',
-			'blog_admin_index_index.php'
+			'blog_admin_posts_index.php'
 		);
 	}
 }
 
-return new \application\BlogFrontIndex();
+return new \application\BlogAdminPosts();
