5ff10012f19a306ca861fa83443f20fc81b24315
Nikola <nikola@localhost.localdomain>
2013-05-08 23:09:59 +0200

    Fetching posts.

--- application/views/blog_admin_posts_index.php
+++ application/views/blog_admin_posts_index.php
@@ -1,25 +1,21 @@
 <table class='show'>
 	<thead>
 		<tr>
-			<th>id</th>
 			<th>name</th>
 			<th>title</th>
 			<th>body</th>
 			<th>created at</th>
 			<th>modified at</th>
-			<th>view</th>
 		</tr>
 	</thead>
 	<tbody>
 		<?php while($post=$values['posts']->fetch()): ?>
 		<tr>
-			<td><?php echo $post->getId(); ?></td>
-			<td><?php echo $post->getName(); ?></td>
+			<td><?php echo \application\classes\ClassFactory::create('Links')->link('','',$post->getName()); ?></td>
 			<td><?php echo \application\classes\ClassFactory::create('Text')->cut($post->getTitle()); ?></td>
 			<td><?php echo \application\classes\ClassFactory::create('Text')->cut($post->getBody()); ?></td>
 			<td><?php echo gmdate('Y-m-d',$post->getCreatedAt()); ?></td>
 			<td><?php echo gmdate('Y-m-d',$post->getModifiedAt()); ?></td>
-			<td><?php echo \application\classes\ClassFactory::create('Links')->link('','','view'); ?></td>
 		</tr>
 		<?php endwhile; ?>
 	</tbody>
