8a83cff8388dd163a99681949f9afd1aba1957ec
Nikola <nikola@localhost.localdomain>
2013-05-19 19:26:12 +0200

    Support for different requests.

--- configure/routes.php
+++ configure/routes.php
@@ -1,35 +1,5 @@
 <?php
 
 return array(
-	'folder'=>'application',
-	'class'=>function(){
-		require_once('premade/LettersFactory.php');
-		$letters=\premade\LettersFactory::create();
-		return $letters->getCorrectClass($_GET['class'],
-			function($word){
-				return ucfirst($word);
-			},
-			'_lettersUnderscoresOnly'
-		);
-	},
-	'action'=>function(){
-		require_once('premade/LettersFactory.php');
-		$letters=\premade\LettersFactory::create();
-		return $letters->getCorrectClass($_GET['action'],
-			function($word){
-				return lcfirst($word);
-			},
-			'_lettersUnderscoresNumbersOnly'
-		);
-	},
-	'params'=>function(){
-		require_once('premade/LettersFactory.php');
-		$letters=\premade\LettersFactory::create();
-		return $letters->getCorrectParams(array_slice($_GET,2),
-			function($word){
-				return strtolower($word);
-			},
-			'_lettersUnderscoresNumbersOnly'
-		);
-	}
+	'folder'=>'application'
 );
