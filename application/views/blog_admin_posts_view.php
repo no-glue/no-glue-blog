a0e41848a7fafe5b9de037e4d5981a15dcf10b6e
Nikola <nikola@localhost.localdomain>
2013-05-25 12:20:15 +0200

    View post.

--- application/views/blog_admin_posts_view.php
+++ application/views/blog_admin_posts_view.php
@@ -6,10 +6,10 @@
 <div class='line title'>body</div>
 <div class='line'><textarea name='body' class='text-area'><?php echo $values['post']->getBody(); ?></textarea></div>
 <div class='line'><input type='hidden' name='created_at' value='<?php echo $values['post']->getCreatedAt(); ?>' /></div>
+<div class='line'><input type='hidden' name='id' value='<?php echo $values['post']->getId(); ?>' /></div>
+<div class='line'><input type='submit' name='update' value='update' class='submit' /></div>
+<?php echo \application\classes\ClassFactory::create('Form')->close(); ?>
 <div class='line title'>created at</div>
 <div class='line'><?php echo date('Y-m-d H:i:s',$values['post']->getCreatedAt()); ?></div>
 <div class='line title'>modified at</div>
 <div class='line'><?php echo date('Y-m-d H:i:s',$values['post']->getModifiedAt()); ?></div>
-<div class='line'><input type='hidden' name='id' value='<?php echo $values['post']->getId(); ?>' /></div>
-<div class='line'><input type='submit' name='update' value='update' class='submit' /></div>
-<?php echo \application\classes\ClassFactory::create('Form')->close(); ?>
