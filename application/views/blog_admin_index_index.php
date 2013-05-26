9e6b21238c7b5c7e79a4de934fffd98f7a0b5b00
Nikola <nikola@localhost.localdomain>
2013-05-26 21:36:46 +0200

    Log in.

--- application/views/blog_admin_index_index.php
+++ application/views/blog_admin_index_index.php
@@ -1,1 +1,7 @@
-blog admin log in
+<?php echo \application\classes\ClassFactory::create('Form')->open('blog_admin_index','login','<div class=\'line\'>','</div>'); ?>
+<div class='line title'>username</div>
+<div class='line'><input type='text' name='username' class='text' /></div>
+<div class='line title'>password</div>
+<div class='line'><input type='password' name='password' class='text' /></div>
+<div class='line'><input type='submit' name='login' value='log in' class='submit' /></div>
+<?php echo \application\classes\ClassFactory::create('Form')->close(); ?>
