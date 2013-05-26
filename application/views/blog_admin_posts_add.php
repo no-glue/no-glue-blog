2e63782fafe49884eedd8892725ba380475857d5
Nikola <nikola@localhost.localdomain>
2013-05-26 11:52:48 +0200

    Adding posts.

--- application/views/blog_admin_posts_add.php
+++ application/views/blog_admin_posts_add.php
@@ -1,1 +1,9 @@
-add post
+<?php echo \application\classes\ClassFactory::create('Form')->open('blog_admin_posts','add','<div class=\'line\'>','</div>'); ?>
+<div class='line title'>name</div>
+<div class='line'><input type='text' name='name' class='text' /></div>
+<div class='line title'>title</div>
+<div class='line'><input type='text' name='title' class='text' /></div>
+<div class='line title'>body</div>
+<div class='line'><textarea name='body' class='text-area'></textarea></div>
+<div class='line'><input type='submit' name='add' value='add' class='submit' /></div>
+<?php echo \application\classes\ClassFactory::create('Form')->close(); ?>
