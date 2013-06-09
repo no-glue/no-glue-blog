41c09f21e92f9ea67b24b7daab62b7893a58f761
Nikola <nikola@localhost.localdomain>
2013-06-09 12:15:33 +0200

    More users for fixtures.

--- application/sql/fixtures.sql
+++ application/sql/fixtures.sql
@@ -8,3 +8,7 @@ INSERT INTO posts(name,title,body,created_at,modified_at) VALUES
 ('far-far-away-something5','Far far away, behind the word mountains, far from the countries Vokalia and Consonantia','Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.',UNIX_TIMESTAMP(),UNIX_TIMESTAMP());
 
 INSERT INTO users (username,password,`level`,created_at,modified_at) VALUES ('admin','admin',0,UNIX_TIMESTAMP(),UNIX_TIMESTAMP());
+INSERT INTO users (username,password,`level`,created_at,modified_at) VALUES ('someone','password',1,UNIX_TIMESTAMP(),UNIX_TIMESTAMP());
+INSERT INTO users (username,password,`level`,created_at,modified_at) VALUES ('anotherone','password',1,UNIX_TIMESTAMP(),UNIX_TIMESTAMP());
+INSERT INTO users (username,password,`level`,created_at,modified_at) VALUES ('adude','password',1,UNIX_TIMESTAMP(),UNIX_TIMESTAMP());
+INSERT INTO users (username,password,`level`,created_at,modified_at) VALUES ('aguy','password',1,UNIX_TIMESTAMP(),UNIX_TIMESTAMP());
