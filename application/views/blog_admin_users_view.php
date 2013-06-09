3783fae5f86de54acf9f8ecf6258ef3fc8553da0
Nikola <nikola@localhost.localdomain>
2013-06-09 12:47:21 +0200

    Viewing user.

--- application/views/blog_admin_users_view.php
+++ application/views/blog_admin_users_view.php
@@ -1,1 +1,13 @@
-view user
+<?php echo \application\classes\ClassFactory::create('Form')->open('blog_admin_users','view','<div class=\'line\'>','</div>'); ?>
+<div class='line title'>username</div>
+<div class='line'><input type='text' name='username' value='<?php echo $values['user']->getUsername(); ?>' class='text' /></div>
+<div class='line title'>password</div>
+<div class='line'><input type='text' name='username' class='text' /></div>
+<div class='line title'>level</div>
+<div class='line'><input type='text' name='username' value='<?php echo $values['user']->getLevel(); ?>' class='text' /></div>
+<div class='line'><input type='submit' name='update' value='update' class='submit' /></div>
+<?php echo \application\classes\ClassFactory::create('Form')->close(); ?>
+<div class='line title'>created at</div>
+<div class='line'><?php echo date('Y-m-d H:i:s',$values['user']->getCreatedAt()); ?></div>
+<div class='line title'>modified at</div>
+<div class='line'><?php echo date('Y-m-d H:i:s',$values['user']->getModifiedAt()); ?></div>
