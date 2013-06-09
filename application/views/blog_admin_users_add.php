bc112149e433a203363bebae217cbc10c45e884e
Nikola <nikola@localhost.localdomain>
2013-06-09 22:00:34 +0200

    Adding users

--- application/views/blog_admin_users_add.php
+++ application/views/blog_admin_users_add.php
@@ -1,1 +1,9 @@
-add user
+<?php echo \application\classes\ClassFactory::create('Form')->open('blog_admin_users','add','<div class=\'line\'>','</div>'); ?>
+<div class='line title'>username</div>
+<div class='line'><input type='text' name='username' class='text' /></div>
+<div class='line title'>password</div>
+<div class='line'><input type='text' name='password' class='text' /></div>
+<div class='line title'>level</div>
+<div class='line'><input type='text' name='level' class='text' /></div>
+<div class='line'><input type='submit' name='add' value='add' class='submit' /></div>
+<?php echo \application\classes\ClassFactory::create('Form')->close(); ?>
