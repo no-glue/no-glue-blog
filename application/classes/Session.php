57190e82aaf7915129766a6afd8d9565add2e5b9
Nikola <nikola@localhost.localdomain>
2013-06-10 21:11:19 +0200

    Added file sessions

--- application/classes/Session.php
+++ application/classes/Session.php
@@ -3,33 +3,28 @@
 namespace application\classes;
 
 class Session{
-	public function __construct(){}
+	protected $_actual;
+
+	public function __construct(
+		$actual=array(
+			'object'=>'\\application\\classes\\SessionFile',
+			'factory_file'=>'SessionFile.php'
+		)
+	){
+		require_once($actual['factory_file']);
+
+		$this->_actual=$actual['object']::instance();
+	}
 
 	public function login($userLevel){
-		require_once('ConfigureLoader.php');
-
-		$accessRights=ConfigureLoader::help('configure/','accessRights.php');
-
-		session_start();
-	
-		$_SESSION['access_rights']=
-			array_slice($accessRights,$userLevel);
-
-		session_regenerate_id();
+		$this->_actual->login($userLevel);
 	}
 
 	public function logout(){
-		session_start();
-
-		unset($_SESSION['access_rights']);
-
-		return TRUE;
+		return $this->_actual->logout();
 	}
 
 	public function currentUserCan($right){
-		session_start();
-
-		return isset($_SESSION['access_rights']) AND
-			in_array($right,$_SESSION['access_rights']);
+		return $this->_actual->currentUserCan($right);
 	}
 }
---
+++ application/classes/SessionFile.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace application\classes;
+
+class SessionFile{
+	public function login($userLevel){
+		require_once('ConfigureLoader.php');
+
+		$accessRights=ConfigureLoader::help('configure/','accessRights.php');
+
+		session_start();
+	
+		$_SESSION['access_rights']=
+			array_slice($accessRights,$userLevel);
+
+		session_regenerate_id();
+	}
+
+	public function logout(){
+		session_start();
+
+		unset($_SESSION['access_rights']);
+
+		return TRUE;
+	}
+
+	public function currentUserCan($right){
+		session_start();
+
+		return isset($_SESSION['access_rights']) AND
+			in_array($right,$_SESSION['access_rights']);
+	}
+
+	public static function instance(){
+		return new \application\classes\SessionFile();
+	}
+}
