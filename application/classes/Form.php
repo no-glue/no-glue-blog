9ed4e808b6b980579e1cf93f79165a13c25befd6
Nikola <nikola@localhost.localdomain>
2013-07-14 12:29:18 +0200

    moved host to constants

--- application/classes/Form.php
+++ application/classes/Form.php
@@ -2,16 +2,19 @@
 
 namespace application\classes;
 
+require_once('premade/Constants.php');
+
+use premade;
+
 class Form{
-	protected static function _helper($helper,$helpers=array(
-		'configure_loader'=>'\application\classes\ConfigureLoader')){
-		return $helpers[$helper];
-	}
-
-	public function open($class,$action,$wrapOpen='',$wrapClose='',$method='post'){
-		$configureLoader=self::_helper('configure_loader');
-		$host=$configureLoader::help()['host'];
-
+	public function open(
+		$class,
+		$action,
+		$wrapOpen='',
+		$wrapClose='',
+		$method='post',
+		$host=\premade\Constants::HOST
+	){
 		$string='<form method=\''.$method.'\' action=\''.$host.'\'>';
 		$string.=$wrapOpen.'<input type=\'hidden\' name=\'class\' value=\''.$class.'\' />'.$wrapClose;
 		$string.=$wrapOpen.'<input type=\'hidden\' name=\'action\' value=\''.$action.'\' />'.$wrapClose;
