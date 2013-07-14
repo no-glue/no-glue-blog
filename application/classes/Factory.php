db4ae8dbdae4576098eb07185eb973c8db919fa3
Nikola <nikola@localhost.localdomain>
2013-07-14 12:02:04 +0200

    class factory

---
+++ application/classes/Factory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace application\classes;
+
+class Factory{
+	public static function create($object,
+	$lookWhere='',
+	$extension='.php',
+	$namespace='\\application\\classes\\',
+	$methodRedirect='redirect'
+	){
+		$require=$lookWhere.$object.$extension;
+		$object=$namespace.$object;
+
+		method_exists($object,$methodRedirect) AND
+		$redirect=$object::redirect() AND
+		$require=$lookWhere.$redirect.$extension AND
+		$redirect=$namespace.$redirect AND
+		$object=$redirect;
+
+		require_once($require);
+
+		return new $object;
+	}
+}
