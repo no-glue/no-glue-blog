162ef017f1f5c8619d525ebaf3bd7015373a6282
Nikola Pavlicevic <nikola@nikola-vi.(none)>
2013-07-26 23:13:03 +0200

    pdo connection one

--- premade/PdoDatabaseConnection.php
+++ premade/PdoDatabaseConnection.php
@@ -3,37 +3,18 @@
 namespace premade;
 
 class PdoDatabaseConnection{
-	protected $_connection;
 	protected static $_instance;
-
-	public function __construct(
-		$driver,
-		$host,
-		$dbname,
-		$username,
-		$password
-	){
-
-		$this->_connection=new \PDO($driver.':host='.$host.
-			';dbname='.$dbname,
-			$username,
-			$password
-		);
-	}
-
-	public function getConnection(){
-		return $this->_connection;
-	}
 
 	public static function getInstance(
 		$driver='mysql',
 		$host='localhost',
 		$dbname='noglue_blog',
 		$username='root',
-		$password='root'
+		$password='root',
+		$real='\\premade\\PdoDatabaseConnectionOne'
 	){
 		if(!self::$_instance){
-			self::$_instance=new \premade\PdoDatabaseConnection(
+			self::$_instance=new $real(
 				$driver,
 				$host,
 				$dbname,
