3039277c7a4fab9fb14a579541d0e6c9ac240e00
Nikola Pavlicevic <nikola@nikola-vi.(none)>
2013-07-28 17:16:55 +0200

    added view to useful

---
+++ useful/View.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace useful;
+
+class View{
+	public static function load($view,
+	$for,
+	$additionalValues=array(),
+	$valuesLocation='application/values/',
+	$viewsLocation='application/views/'){
+		$values=require_once($valuesLocation.$view);
+		$forValues=require_once($valuesLocation.$for);
+
+		$values['subviews']=
+			array_merge($values['subviews'],$forValues);
+
+		!empty($additionalValues) AND $values=array_merge($values,$additionalValues);
+
+		require_once('Factory.php');
+
+		require_once($viewsLocation.$view);
+	}
+}
