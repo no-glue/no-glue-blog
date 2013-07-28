db3df70ceb1e2366a9de7f9c402a034d02f381ab
Nikola Pavlicevic <nikola@nikola-vi.(none)>
2013-07-28 16:34:44 +0200

    added form to useful

--- application/views/blog_admin_posts_add.php
+++ application/views/blog_admin_posts_add.php
@@ -1,4 +1,4 @@
-<?php echo \application\classes\Factory::create('Form')->open('blog_admin_posts','add','<div class=\'line\'>','</div>'); ?>
+<?php echo \useful\Factory::create('Form')->open('blog_admin_posts','add','<div class=\'line\'>','</div>'); ?>
 <div class='line title'>name</div>
 <div class='line'><input type='text' name='name' class='text' /></div>
 <div class='line title'>title</div>
@@ -6,4 +6,4 @@
 <div class='line title'>body</div>
 <div class='line'><textarea name='body' class='text-area'></textarea></div>
 <div class='line'><input type='submit' name='add' value='add' class='submit' /></div>
-<?php echo \application\classes\Factory::create('Form')->close(); ?>
+<?php echo \useful\Factory::create('Form')->close(); ?>
--- application/views/blog_admin_users_view.php
+++ application/views/blog_admin_users_view.php
@@ -1,4 +1,4 @@
-<?php echo \application\classes\Factory::create('Form')->open('blog_admin_users','view','<div class=\'line\'>','</div>'); ?>
+<?php echo \useful\Factory::create('Form')->open('blog_admin_users','view','<div class=\'line\'>','</div>'); ?>
 <div class='line title'>username</div>
 <div class='line'><input type='text' name='username' value='<?php echo $values['user']->getUsername(); ?>' class='text' /></div>
 <div class='line title'>password</div>
@@ -8,7 +8,7 @@
 <div class='line'><input type='hidden' name='created_at' value='<?php echo $values['user']->getCreatedAt(); ?>' /></div>
 <div class='line'><input type='hidden' name='id' value='<?php echo $values['user']->getId(); ?>' /></div>
 <div class='line'><input type='submit' name='update' value='update' class='submit' /></div>
-<?php echo \application\classes\Factory::create('Form')->close(); ?>
+<?php echo \useful\Factory::create('Form')->close(); ?>
 <div class='line title'>created at</div>
 <div class='line'><?php echo date('Y-m-d H:i:s',$values['user']->getCreatedAt()); ?></div>
 <div class='line title'>modified at</div>
