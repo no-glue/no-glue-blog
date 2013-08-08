eb2b787b0e138be2271f10551e150dfa85f0166b
Nikola Pavlicevic <nikola@nikola-vi.(none)>
2013-08-08 20:55:37 +0200

    cut text

--- useful/Text.php
+++ useful/Text.php
@@ -4,6 +4,15 @@ namespace useful;
 
 class Text{
 	public function cut($text,$length=100,$start=0){
-		return substr($text,$start,$length);
+		$result=substr($text,$start,$length);
+		$lastch=$result[strlen($result)-1];
+
+		$lastch==='.' AND
+		$result.='..';
+
+		$lastch!=='.' AND
+		$result.='...';
+
+		return $result;
 	}
 }
