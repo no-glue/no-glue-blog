00558f0e19d4e22aa35ff0a573e71b0ee32af36c
Nikola Pavlicevic <nikola@nikola-vi.(none)>
2013-08-11 19:29:25 +0200

    running fiddles from single point

---
+++ fiddles/Database.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace fiddles;
+
+require_once('../premade/SplClassLoader.php');
+
+class Database{
+	public function __construct(){}
+
+	public function make(
+		$database='',
+		$databaseWrapper='PdoDatabaseWrapper'
+	){
+	}
+}
---
+++ fiddles/Index.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace fiddles;
+
+class Index{
+	public function __construct(
+		$classLoader='../premade/SplClassLoader.php',
+		$folder=__DIR__
+	){
+		$loader=require_once($classLoader);
+		$loader->setIncludePath(dirname($folder));
+	}
+
+	public function run($argv,$namespace='\\fiddles\\'){
+		$argv=array_slice($argv,1);
+
+		$object=array_shift($argv);
+
+		$object=$namespace.$object;
+
+		$function=array_shift($argv);
+
+		print_r($argv);
+
+		call_user_func_array(array(new $object,$function),$argv);
+
+		return $this;
+	}
+}
+
+(new \fiddles\Index())->run($argv);
