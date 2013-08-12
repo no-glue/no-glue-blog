8eb26e0f3e49239430c4d104d2349e94e3b257c6
Nikola Pavlicevic <nikola@nikola-vi.(none)>
2013-08-12 22:52:02 +0200

    new lines

--- blog/views/blog_front_index_index.php
+++ blog/views/blog_front_index_index.php
@@ -1,5 +1,5 @@
 <?php while($post=$values['posts']->fetch()): ?>
 <div class='line title'><?php echo $post->getTitle();?></div>
-<div class='line'><?php echo \useful\Factory::create('Text')->cut($post->getBody(),300);?></div>
+<div class='line'><?php echo nl2br(\useful\Factory::create('Text')->cut($post->getBody(),300);)?></div>
 <div class='line'><?php echo \useful\Factory::create('Link')->link('blog_front_index','more','more',array('id'=>$post->getId()));?></div>
 <?php endwhile; ?>
--- blog/views/blog_front_index_more.php
+++ blog/views/blog_front_index_more.php
@@ -1,2 +1,2 @@
 <div class='line title'><?php echo $values['post']->getTitle();?></div>
-<div class='line'><?php echo $values['post']->getBody();?></div>
+<div class='line'><?php echo nl2br($values['post']->getBody();)?></div>
