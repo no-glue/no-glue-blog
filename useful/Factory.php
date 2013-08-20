e20b3be7bb4f4e54e39143a66c76673a497074c4
Nikola Pavlicevic <nikola@nikola-vi.(none)>
2013-08-20 22:11:39 +0200

    simplified factory

--- useful/Factory.php
+++ useful/Factory.php
@@ -3,21 +3,17 @@
 namespace useful;
 
 class Factory{
-	public static function create($object,
-	$lookWhere='',
-	$extension='.php',
-	$namespace='\\useful\\',
-	$methodRedirect='another'
+	public static $real=array(
+		'Session'=>'SessionDatabase'
+	);
+	
+	public static function create(
+		$object,
+		$lookWhere='',
+		$namespace='\\useful\\'
 	){
-		$require=$lookWhere.$object.$extension;
-
+		$object=isset(self::$real[$object])?self::$real[$object]:$object;
 		$object=$namespace.$object;
-
-		method_exists($object,$methodRedirect) AND
-		$redirect=$object::$methodRedirect() AND
-		$require=$lookWhere.$redirect.$extension AND
-		$redirect=$namespace.$redirect AND
-		$object=$redirect;
 
 		return new $object;
 	}
