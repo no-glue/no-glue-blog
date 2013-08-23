e4059018f849639e16bfe0a4a75925a92da7e6a2
Nikola Pavlicevic <nikola@nikola-vi.(none)>
2013-08-23 22:24:50 +0200

    think act blog admin index

--- blog/BlogAdminIndex.php
+++ blog/BlogAdminIndex.php
@@ -3,18 +3,21 @@
 namespace blog;
 
 class BlogAdminIndex{
-	public function __construct(){}
+	protected $think;
+	protected $act;
+
+	public function __construct(
+		$think='BlogAdminIndexThink',
+		$thinkFactory='\\blog\\think\\Factory',
+		$act='BlogAdminIndexAct',
+		$actFactory='\\blog\\act\\Factory'
+	){
+		$this->think=$thinkFactory::create($think);
+		$this->act=$actFactory::create($act);
+	}
 
 	public function index($requestType,$requestObject){
-		$requestType===\premade\Constants::REQUEST_POST AND
-		$user=\blog\models\Factory::create('UserDao') AND
-		$count=$user->login(
-			$requestObject->username,
-			$requestObject->password
-		) AND
-		$user->getSession()->currentUserCan('can_access_admin') AND
-		\useful\Factory::create('Redirect')
-			->redirect('blog_admin_posts','index');
+		$this->think->canAccessAdmin($requestType,$requestObject->username,$requestObject->password) AND $this->act->redirect('blog_admin_posts','index');
 
 		\useful\View::load('blog_admin_template.php',
 			'blog_admin_index_index.php'
