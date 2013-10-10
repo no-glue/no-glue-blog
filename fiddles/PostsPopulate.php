199e56e1bed47015f3bba40ce28d4a7edf628636
Nikola Pavlicevic <nikola_pav@eunet.rs>
2013-10-10 20:34:57 +0000

    populating posts

--- fiddles/PostsPopulate.php
+++ fiddles/PostsPopulate.php
@@ -5,18 +5,37 @@ namespace fiddles;
 class PostsPopulate{
 	public function populate(
 		$posts=16,
+		$nameChars=16,
+		$bodyChars=256,
 		$act='BlogAct',
 		$fakeLorem='FakeLorem',
-		$factoryAct='\blog\act\facory',
+		$factoryAct='\blog\act\Factory',
 		$factoryUseful='\useful\Factory'
 	){
 		$act=$factoryAct::create($act);
 
 		$fakeLorem=$factoryUseful::create($fakeLorem);
 
-		while($posts--){
-		}
+		$createdAt=time();
 
-		var_dump($fakeLorem->text());
+		while($posts--){
+			$name=$fakeLorem->text(
+				$nameChars
+			);
+
+			$body=$fakeLorem->text(
+				$bodyChars
+			);
+
+			$act->addPost(
+				(Object)array(
+					'name'=>str_replace(' ','-',$name),
+					'title'=>$name,
+					'body'=>$body,
+					'created_at'=>$createdAt,
+					'modified_at'=>$createdAt
+				)
+			);
+		}
 	}
 }
